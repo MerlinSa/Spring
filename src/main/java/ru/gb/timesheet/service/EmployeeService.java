1b43a36b50ff2792b42bbfb9b5d5e47a6cffd50d
Saikala <s.borubaevaa@gmail.com>
2024-07-25 16:40:47 +0600

    "homework#5 Spring"

---
+++ src/main/java/ru/gb/timesheet/service/EmployeeService.java
@@ -0,0 +1,37 @@
+package ru.gb.timesheet.service;
+
+import org.springframework.stereotype.Service;
+import ru.gb.timesheet.model.Employee;
+import ru.gb.timesheet.model.Project;
+import ru.gb.timesheet.model.Timesheet;
+import ru.gb.timesheet.repository.EmployeeRepository;
+import ru.gb.timesheet.repository.ProjectRepository;
+import ru.gb.timesheet.repository.TimesheetRepository;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+@Service
+public class EmployeeService {
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public Optional<Employee> findById(Long id) {
+        return employeeRepository.findById(id);
+    }
+
+    public List<Employee> findAll() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee create(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public void delete(Long id) {
+        employeeRepository.deleteById(id);
+    }
+}
